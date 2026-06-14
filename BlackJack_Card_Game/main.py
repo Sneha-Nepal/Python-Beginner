333589f3396900c2c70a685ed6d09a3bbdfe74d8
Sneha-Nepal <snehisneha02@gmail.com>
2026-06-14 15:52:05 +0545

    Add comments in main.py file

--- BlackJack_Card_Game/main.py
+++ BlackJack_Card_Game/main.py
@@ -1,14 +1,17 @@
 import random
 
+# there is no 1. For ace is only 11
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 print("Welcome to BlackJack Card Game!")
 start= input("Would you like to start?'y' or 'n': ")
 
+# to store the cards of user(initials and the ones being added)
 comp_cards = [] 
 user_cards = []
 
 def deal_initial_cards():
+    # stating two cards of dealer and user, but dealer second card is hidden
 
     for _ in range(2):
         comp1 = random.choice(cards)
@@ -23,6 +26,8 @@ def deal_initial_cards():
 
 
 def add_cards(player_cards):
+    # add cards of user and dealer
+
     another_card = random.choice(cards)
     player_cards.append(another_card)
 
@@ -34,10 +39,13 @@ def add_cards(player_cards):
     print(f"Your Card: {user_cards}")
 
 def calculator(lst):
+    # helper function to calculate sum
     return sum(lst)
 
 
 def final_score(total_user, total_comp):
+    # to calculate the final score and decide result
+
     print(f"Your Total: {total_user}")
     print(f"Dealer Total: {total_comp}")
     
@@ -55,12 +63,14 @@ def final_score(total_user, total_comp):
 
 
 if start == 'y':
+    #Stating two cards i.e. game start
     deal_initial_cards()
 
     go_again = True
     while go_again:
         next_move = input("'hit' or 'stand': ")
 
+        # add cards to the user
         if next_move == 'hit':
             add_cards(user_cards)
             total_user = calculator(user_cards)
@@ -68,6 +78,7 @@ if start == 'y':
                 print("You went over 21!")
                 go_again =False
 
+        # dealer add cards 
         elif next_move == 'stand':
             while sum(comp_cards) < 17:
                 add_cards(comp_cards)
